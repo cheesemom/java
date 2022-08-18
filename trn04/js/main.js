08acb7fbfdff692f2b4423e95a5c3b10ef0a3592
cheesemom <smsm2610@naver.com>
2022-08-18 17:15:49 +0900

    0818

--- trn04/js/main.js
+++ trn04/js/main.js
@@ -5,7 +5,11 @@ window.addEventListener('DOMContentLoaded', () => {
         console.log(sct);
         sct > 0
             ? document.querySelector('#Header').classList.add('on')
-            : document.querySelector('#Header').classList.remove('on')
+            : document.querySelector('#Header').classList.remove('on');
+
+        sct > 500
+            ? document.querySelector('.to_top').classList.add('on')
+            : document.querySelector('.to_top').classList.remove('on');
     });
 
     const MainSlider = new Swiper('.MainSlider', {
@@ -31,6 +35,20 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    document.querySelector('.to_top').addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    });
+
+
+    document.querySelector('.mopen').addEventListener('click', (e) => {
+        const TG = e.currentTarget;
+        TG.classList.toggle('on');
+        document.querySelector('#Header').classList.toggle('ox')
+    });
+
+    document.querySelector('#Header').addEventListener('wheel', (e) => {
+        e.preventDefault() //이벤트 자체를 막음...
+    })
 
 
 })
